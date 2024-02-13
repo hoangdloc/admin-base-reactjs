7afa652882dd7f1a289ab938fd99170ffd3207d2
Hoang Loc <hoanggdloc@gmail.com>
2024-02-14 02:22:26 +0700

    Update react eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,62 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+      1,
+      {
+        allowConstantExport: true,
+      },
     ],
+    'react/button-has-type': 1,
+    'react/react-in-jsx-scope': 0,
+    'react/self-closing-comp': [
+      2,
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'react/jsx-tag-spacing': 2,
+    'react/jsx-sort-props': [
+      1,
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        multiline: 'last',
+        reservedFirst: ['key', 'ref'],
+      },
+    ],
+    'react/sort-default-props': 1,
+    'react/jsx-no-leaked-render': 1,
+    'react/jsx-handler-names': 1,
+    'react/jsx-no-useless-fragment': [
+      1,
+      {
+        allowExpressions: true,
+      },
+    ],
+    'react/jsx-no-duplicate-props': 2,
+    'react/hook-use-state': [
+      1,
+      {
+        allowDestructuredState: true,
+      },
+    ],
+    'no-console': 2,
   },
-}
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  printWidth: 100,
+  singleAttributePerLine: true,
+  useTabs: false,
+  tabWidth: 2,
+  endOfLine: 'auto',
+};
