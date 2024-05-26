60008040569583c3c2dbfb973a31e9c2eb10fc29
Hoang Loc <hoanggdloc@gmail.com>
2024-05-26 23:44:11 +0700

    Feature: Add collapsible component

---
+++ src/components/atoms/collapsible/Collapsible.tsx
@@ -0,0 +1,15 @@
+import * as CollapsiblePrimitive from '@radix-ui/react-collapsible';
+
+const CollapsibleRoot = CollapsiblePrimitive.Root;
+
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
+
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+
+const Collapsible = {
+  Content: CollapsibleContent,
+  Root: CollapsibleRoot,
+  Trigger: CollapsibleTrigger,
+};
+
+export { Collapsible };
